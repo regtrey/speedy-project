b6bff8b2adc34c9a37ff3df2ecf869acd54b99e5
regtrey <manigosreggie@gmail.com>
2022-03-19 15:05:26 +0800

    config

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,7 @@ mongoose
   })
   .then((con) => console.log('DB connection successful!'));
 
-const port = process.env.PORT || 5000;
-const server = app.listen(port, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
   console.log(`App running on port ${port}...`);
 });
 
